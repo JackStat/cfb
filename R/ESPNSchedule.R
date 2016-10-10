8d320392cb728fb06b2f4f3ede0ae9bc08db4033
Tyler Hunt <tyler.hunt@utah.edu>
2016-10-09 20:00:21 -0600

    added ESPNSchedule and reworked ESPNResults

--- R/ESPNSchedule.R
+++ R/ESPNSchedule.R
@@ -1,14 +1,13 @@
-#' @title Scrape ESPN Game Results.
-#' @description Using curl we pull all 254 teams schedules at once. Use this function sparingly as is hits ESPN's servers in an intense way. Takes about 25 seconds
+#' @title Scrape ESPN Schedules
+#' @description Scrape and clean up schedules into a \code{data.frame}
 #' 
-#' @import XML curl dplyr
-#' @importFrom tidyr separate
+#' @import dplyr
 #' @importFrom jsonlite fromJSON
 #' 
 #' @export
 
 
-ESPNResults <- function(Year = 2016, ESPNSource = NULL) {
+ESPNSchedule <- function(Year = 2016, ESPNSource = NULL) {
   
   if(is.null(ESPNSource)){
     ESPNSource <- ESPNGET(Year)
@@ -16,7 +15,7 @@ ESPNResults <- function(Year = 2016, ESPNSource = NULL) {
   
   IdJSON <- function(x){
     # Basically give me everything between { and } but keep the {}
-    g1 <- gsub("(.*) \\{(.*)\\} (.*)","\\{\\2\\}", x)
+    g1 <- gsub("(.*) \\{(.*)\\} (.*)","\\{\\2\\}",x)
     # add in leading double quotes so we can convert form JSON
     g2 <- gsub("(\\{| )([a-z]{1,})", '\\1"\\2', g1)
     # add in trailing double quotes so we can convert form JSON
@@ -41,47 +40,32 @@ ESPNResults <- function(Year = 2016, ESPNSource = NULL) {
   })
   
   dirtyGames <- do.call(rbind, CC)
-  
-  cleanGames<-
-  dirtyGames %>% 
-    mutate(
+    
+  Schedule <-
+    dirtyGames %>% 
+    dplyr::mutate(
       Home = as.numeric(substr(X2, 1, 2) == 'vs')
       # - Need to remove anything after a space just in case there is OT
       ,X3 = gsub(' [A-z0-9 ]*', '' , X3)
-      ,Score = substr(X3, 2, 20)
       # - 1st char in either W or L for the games that have already been played
       ,Result = as.factor(substr(X3, 1, 1))
       # - Removing the vs or @
       ,Opponent = espnOpp(X2)
-      ,Date = espnDate(paste(X1, Year))
       ,Year = substr(Info, 1, 4)
+      ,Date = espnDate(paste(X1, Year))
     ) %>%
     # - Keep games that haven't happened yet and have a Date
-    filter(Result %in% c("W", "L")) %>%
-    separate(Score, c("PF1", "PA1")) %>%
-    select(
+    dplyr::filter(!Result %in% c("W", "L") & !is.na(Date)) %>%
+    dplyr::select(
       Date
       ,Home
-      ,PF1
-      ,PA1
       ,Opponent
-      ,Result
       ,Team
       ,Year
-      ,espnID = teamId
-      ,conferenceID = groupId
     )
-
-  cleanGames <- Numerics(cleanGames)
   
-  cleanGames$PF[cleanGames$Result=='L'] = cleanGames$PA1[cleanGames$Result=='L']
-  cleanGames$PA[cleanGames$Result=='L'] = cleanGames$PF1[cleanGames$Result=='L']
-  cleanGames$PF[cleanGames$Result=='W'] = cleanGames$PF1[cleanGames$Result=='W']
-  cleanGames$PA[cleanGames$Result=='W'] = cleanGames$PA1[cleanGames$Result=='W']
+  Schedule <- Numerics(Schedule)
   
-  cleanGames <- select(cleanGames, -PF1, -PA1)
-  
-  return(cleanGames)
-  
+  return(Schedule)
 }
 
--- R/espnOpp.R
+++ R/espnOpp.R
@@ -22,3 +22,26 @@ espnDate<-function(x){
     # -converting to date
     as.Date(fixSept, "%b %d %Y")
 }
+
+
+
+Numerics <- function(data){
+  findNums<-function(x) apply(x,2, FUN = function(x){!all(grepl('[^0-9]', x))})
+
+  ff<-findNums(data)
+  
+  if(sum(ff) == 0){
+    data
+  }
+  if(sum(ff) == 1){
+    data[,ff] <- as.numeric(data[,ff])
+    data
+  }else{
+    convert <- function(x) apply(x, 2, as.numeric)
+  
+    data[,ff] <- convert(data[,ff])
+  
+    data
+  }
+
+}
