3319ce3ab20c67865c0b3d7296873366c05eede4
Tyler Hunt <tyler.hunt@utah.edu>
2016-10-26 21:08:29 -0600

    adding scoringSummary function

--- R/NCAAscoringSummary.R
+++ R/NCAAscoringSummary.R
@@ -10,11 +10,21 @@ NCAAscoringSummary <- function(url){
 
   rawfromJSON <- fromJSON(gsub("jsonp", "sites/default/files/data", url))
   
+  meta <- rawfromJSON$meta$teams
+  
   Rows <- sapply(rawfromJSON$periods$summary, nrow)
   
-  data.frame(
-    quarter = rep(rawfromJSON$periods$title, Rows)
-    ,do.call("rbind", rawfromJSON$periods$summary)
-  )
+  if(length(Rows) == 0){
+    NULL
+  } else {
+    data.frame(
+      quarter = rep(rawfromJSON$periods$title, Rows)
+      ,do.call("rbind", rawfromJSON$periods$summary)
+      ,date = as.Date(gsub("(.*)\\/([0-9]{4,})\\/([0-9]{2,})\\/([0-9]{2,})(.*)", "\\2-\\3-\\4", url))
+      ,vsText = gsub("(.*)\\/([0-9]{4,})\\/([0-9]{2,})\\/([0-9]{2,})\\/(.*)\\/scoringSummary.json", "\\5", url)
+    ) %>%
+      left_join(meta, by = c("teamId" = "id"))
+  }
+  
 }
 
